ec9613550a185a466bf19f9425c1d307ea2466af
Suraj Jaiswal <surajjaiswal97@gmail.com>
2026-07-28 19:48:12 +0530

    api not working

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
+import express from 'express';
 import serverless from 'serverless-http';
 import { createApp } from '../server/src/app.js';
 
-const app = createApp();
+const app = express();
+app.use('/api', createApp());
 
 export default serverless(app);
